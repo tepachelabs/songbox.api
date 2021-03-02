5c3221641f3c855c52c15b03804b4079008c3c9e
tonymtz <atony.mtz@gmail.com>
2021-03-01 19:00:03 -0700

    Fixes cors issue

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
 const sassMiddleware = require('node-sass-middleware');
 
 const authRouter = require('./routes/auth');
@@ -16,7 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const meRouter = require('./me');
 const pathRouter = require('./path');
 const streamLinkRouter = require('./stream-link');
@@ -6,6 +8,7 @@ const favoritesRouter = require('./favorites');
 
 const router = express.Router();
 
+router.options('*', cors({ origin: 'https://songbox.io'}));
 router.use('/me', meRouter);
 router.use('/path', pathRouter);
 router.use('/stream_link', streamLinkRouter);
